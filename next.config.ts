1ca8ab48070adbfa1b2b64f11a7a59c90d8d9b0e
Eber Junior <eberssjunior12@gmail.com>
2026-05-11 19:07:19 -0300

    config github pages pt2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "/ra1461392321024";
+
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: repo,
+  assetPrefix: repo,
+
   images: {
     unoptimized: true,
   },
